4f2c3ad5b29a543c7d90c45c5455b67946d8cb0c
haani0090 <haani0090@gmail.com>
2022-05-22 02:19:04 -0700

    account update error resolved

--- src/components/topbar/TopBar.jsx
+++ src/components/topbar/TopBar.jsx
@@ -5,7 +5,7 @@ import "./topbar.css";
 
 export default function TopBar() {
   const { user, dispatch } = useContext(Context);
-  const PF = "https://iznaworld.herokuapp.com/api/images/"
+  const PF = "https://iznaworld.herokuapp.com/images/";
 
   const handleLogout = () => {
     dispatch({ type: "LOGOUT" });
