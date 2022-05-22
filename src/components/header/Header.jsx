42bfbf35b1df93f4e9d69d7f70d3c34b29930c2e
haani0090 <haani0090@gmail.com>
2022-05-22 02:22:25 -0700

    minor bugs

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,7 +4,7 @@ export default function Header() {
   return (
     <div className="header">
       <div className="headerTitles">
-        <span className="headerTitleSm">React & Node</span>
+        <span className="headerTitleSm">Welcome To World Of </span>
         <span className="headerTitleLg">Izna Blog🔥</span>
       </div>
       <img
